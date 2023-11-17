f6510e6f8c6c63221113a5043aa63e944c84671f
stefanuspet <stefanuspetra.p@gmail.com>
2023-11-17 13:27:55 +0700

    initial commit

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang;
+use Illuminate\Http\Request;
+
+class BarangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            $barang = Barang::all();
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil ambil data',
+                "data" => $barang
+            ], 200); //status code 200 = success
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "massage" => $e->getMessage(),
+                "data" => []
+            ], 400); //status code 400 = bad request
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            //$request->all() untuk mengambil semua input dalam request body
+            $barang = Barang::create($request->all());
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil ambil data',
+                "data" => $barang
+            ], 200); //status code 200 = success
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "massage" => $e->getMessage(),
+                "data" => []
+            ], 400); //status code 400 = bad request
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        try {
+            $barang = Barang::find($id);
+
+            if (!$barang) throw new \Exception("Barang tidak ditemukan");
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil ambil data',
+                "data" => $barang
+            ], 200); //status code 200 = success
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "massage" => $e->getMessage(),
+                "data" => []
+            ], 400); //status code 400 = bad request
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $barang = Barang::find($id);
+
+            if (!$barang) throw new \Exception("Barang tidak ditemukan");
+
+            $barang->update($request->all());
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil ambil data',
+                "data" => $barang
+            ], 200); //status code 200 = success
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "massage" => $e->getMessage(),
+                "data" => []
+            ], 400); //status code 400 = bad request
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        try {
+            $barang = Barang::find($id);
+
+            if (!$barang) throw new \Exception("Barang tidak ditemukan");
+
+            $barang->delete();
+
+            return response()->json([
+                "status" => true,
+                "message" => 'Berhasil ambil data',
+                "data" => $barang
+            ], 200); //status code 200 = success
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "massage" => $e->getMessage(),
+                "data" => []
+            ], 400); //status code 400 = bad request
+        }
+    }
+}
